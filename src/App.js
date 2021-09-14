f2c274244bbfb84287aa44459d64a532052032f8
Neal <85963924+NealLyonsWake@users.noreply.github.com>
2021-09-14 21:32:20 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>TastyTV Module 2 Project</h1>
+      <h2>Wow, such empty!</h2>
+    </div>
+  );
+}
